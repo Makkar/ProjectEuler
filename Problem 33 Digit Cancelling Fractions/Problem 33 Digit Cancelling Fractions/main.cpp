738044e0d54e7f192e2b60a84d8925bec5267a8d
Aditya Makkar <adityamakkar001@gmail.com>
2014-05-29 13:01:00 +0530

    Created ProjectEuler Repo

---
+++ Problem 33 Digit Cancelling Fractions/Problem 33 Digit Cancelling Fractions/main.cpp
@@ -0,0 +1,45 @@
+//         /\_/\
+//   _____/ o o \
+//  /~____  =ø= /
+// (______)__m_m)
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <deque>
+#include <set>
+#include <map>
+#include <utility>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <climits>
+#include <limits>
+#include <cstring>
+
+#define pb push_back
+#define pf push_front
+#define all(c) c.begin(), c.end()
+#define tr(c, it) \
+for(typeof(c.begin()) it = c.begin(); it!=c.end(); ++it)
+#define present(container, element) (find(all(container),element) != container.end())
+#define present2(c,x) ((c).find(x) != (c).end()) // For maps and set
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+
+const long double PI = 3.141592653589793238462643383;
+const ll MOD = 1000000007;
+const double EPS = 1e-9; // a==b is abs(a-b)<EPS, a>=b is a>b-EPS, a>b is a>=b+EPS
+const int MAX_INT = 2147483647;
+
+using namespace std;
+
+int main()
+{
+    
+}
